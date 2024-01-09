87ec0c28133801a7e49558dd768499f44c6653c7
achmad.haris <harisyusufputra@gmail.com>
2024-01-09 14:29:29 +0800

    membuat website kedai kopi kenangan senja

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Toggle class active( toggle: tadinya ada, jadi gak ada dan sebaliknya)
+// ambil elemen navbar nav
+const navbarNav = document.querySelector(".navbar-nav");
+
+// ketika hamburger menu diklik
+document.querySelector("#hamburger-menu").onclick = () => {
+  navbarNav.classList.toggle("active");
+};
+
+// Klik di luar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector("#hamburger-menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
+
+// End js bagian navbar
